32cb5b94eff34b0e3bbb4c9cf6dbf1fc674acdcd
balajimunagala <balajimunagala07@gmail.com>
2026-04-10 17:13:52 +0530

    Fix backend CORS and production database connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 const User = require("./models/user_model");
-require("node:dns").setServers(["8.8.8.8", "1.1.1.1"]);
 
 
 connectDB()
